227119ad494860095c7a4fa200a375fc2d0fdf9e
uNiversaI <uNiversal@noreply.github.com>
2015-06-03 16:53:59 +0900

    fix includes

--- src/client.cpp
+++ src/client.cpp
@@ -21,8 +21,8 @@
  */
 
 #include "client.h"
-#include "kodi/xbmc_pvr_dll.h"
-#include "kodi/libKODI_guilib.h"
+#include "xbmc_pvr_dll.h"
+#include "libKODI_guilib.h"
 #include <stdlib.h>
 #include "VuData.h"
 #include "platform/util/util.h"
