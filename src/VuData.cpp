2228081e462a5309a66463b38d40e69d0cfbec15
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2016-03-08 20:39:43 +0100

    [API 5.1.0] PVR_RECORDING.channelType dummy implementation.

--- src/VuData.cpp
+++ src/VuData.cpp
@@ -1366,6 +1366,9 @@ void Vu::TransferRecordings(ADDON_HANDLE handle)
     /* TODO: PVR API 5.0.0: Implement this */
     tag.iChannelUid = PVR_CHANNEL_INVALID_UID;
 
+    /* TODO: PVR API 5.1.0: Implement this */
+    tag.channelType = PVR_RECORDING_CHANNEL_TYPE_UNKNOWN;
+
     PVR->TransferRecordingEntry(handle, &tag);
   }
 }
