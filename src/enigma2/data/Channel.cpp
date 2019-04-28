bcc0a1aa11163aa270a4cf304146cddc93730c55
phunkyfish <phunkyfish@gmail.com>
2019-04-28 09:44:31 +0100

    Store program stream number for channel

--- src/enigma2/data/Channel.cpp
+++ src/enigma2/data/Channel.cpp
@@ -68,7 +68,9 @@ bool Channel::UpdateFrom(TiXmlElement* channelNode)
   if (Settings::GetInstance().UseStandardServiceReference())
     m_serviceReference = m_standardServiceReference;
 
-  Logger::Log(LEVEL_DEBUG, "%s: Loaded Channel: %s, sRef=%s, picon: %s", __FUNCTION__, m_channelName.c_str(), m_serviceReference.c_str(), m_iconPath.c_str());
+  std::sscanf(m_serviceReference.c_str(), "%*X:%*X:%*X:%X:%*s", &m_streamProgramNumber);
+
+  Logger::Log(LEVEL_DEBUG, "%s: Loaded Channel: %s, sRef=%s, picon: %s, program number: %d", __FUNCTION__, m_channelName.c_str(), m_serviceReference.c_str(), m_iconPath.c_str(), m_streamProgramNumber);
 
   m_m3uURL = StringUtils::Format("%sweb/stream.m3u?ref=%s", Settings::GetInstance().GetConnectionURL().c_str(), WebUtils::URLEncodeInline(m_serviceReference).c_str());
 
